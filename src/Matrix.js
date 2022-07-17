8e8cdd5723baa00adab299954742433dba4b8897
Sheena Bonilla <sheena.myb@gmail.com>
2022-07-16 21:06:14 -0700

    commit tree 6177e6dbe86b03c5ebc376157398511c2e5fa000

--- src/Matrix.js
+++ src/Matrix.js
@@ -7,22 +7,34 @@ export default class Matrix extends Component {
 
   constructor() {
     super()
+    this.state = {
+      selectedColor: '#FFF'
+    }
   }
 
+  // updates selectedColor with whatever is passsed into it as an argument
+  setSelectedColor = (newColor) => {
+    this.setState({
+      selectedColor: newColor
+    })
+  }
+
+  // returns a collection of Cell components
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} selectedColor={this.state.selectedColor}/>)
   )
 
   genMatrix = () => (
     this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
   )
 
-  render() {
+  render() { // renders a div containing ColorSelector component and another div
     return (
-      <div id="app">
-        <ColorSelector />
-        <div id="matrix">
-          {this.genMatrix()}
+      <div id="app"> 
+        <ColorSelector setSelectedColor={this.setSelectedColor}/>
+
+        <div id="matrix"> 
+          {this.genMatrix()} 
         </div>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -4,4 +4,7 @@ import './index.css';
 import learnSymbol from './data.js'
 import Matrix from './Matrix.js'
 
+// Matrix component is rendered
+// passes a values prop
+// data used to create the divs come from src/data.js and stored as an array of arrays of strings
 ReactDOM.render(<Matrix values={learnSymbol} />, document.getElementById('root'));
